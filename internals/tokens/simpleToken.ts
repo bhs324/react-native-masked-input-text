e3654dbe0c8ab3a22bbef6caac7c0a907e503c5c
bhs <bowenshin@11dims.com>
2020-12-04 13:47:06 +0900

    한글 추가

--- internals/tokens/simpleToken.ts
+++ internals/tokens/simpleToken.ts
@@ -11,10 +11,10 @@ export default class SimpleToken extends Token {
 	public getRegex(): string {
 		switch (this.tokenValue) {
 			case '0': return '[0-9]';
-			case 'x': return '[a-z]';
-			case 'X': return '[A-Z]';
-			case 's': return '[a-zA-Z]';
-			case 'a': return '[a-zA-Z0-9]';
+			case 'x': return '[a-z가-힣]';
+			case 'X': return '[A-Z가-힣]';
+			case 's': return '[a-zA-Z가-힣]';
+			case 'a': return '[a-zA-Z가-힣0-9]';
 			default: return `\\${this.tokenValue}`;
 		}
 	}
